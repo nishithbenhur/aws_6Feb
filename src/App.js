47bc27c97d6f65b2dc63e7874edb7cd48f8fbe93
EC2 Default User <ec2-user@ip-172-31-51-223.ec2.internal>
2021-02-06 08:43:11 +0000

    update3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>sdsd</p>
+       <p>New update3 React app</p>
         <a
           className="App-link"
           href="https://reactjs.org"
